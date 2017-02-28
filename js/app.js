c65d2bff589bb2fcf38e037aa76e3756105bf63b
Jakub Siwiński <kubsiw@gmail.com>
2017-02-28 23:04:26 +0100

    add contact section with google map indicating the place of studio

--- js/app.js
+++ js/app.js
@@ -4,4 +4,27 @@ jQuery(document).ready(function(){
             interval: 4000 //changes the speed
     });
     
+    
+    
+    jQuery(function ($) {
+        function init_map1() {
+            var myLocation = new google.maps.LatLng(51.818301, 19.420385);
+            var mapOptions = {
+                center: myLocation,
+                zoom: 17
+            };
+            var marker = new google.maps.Marker({
+                position: myLocation,
+                title: "Property Location"
+            });
+            var map = new google.maps.Map(document.getElementById("map1"),
+                mapOptions);
+            marker.setMap(map);
+        }
+        init_map1();
+    });
+    
+    
+    
+    
 });
